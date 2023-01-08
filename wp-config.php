a8ae1102a3166fbb57c84ff296f42e1fdd42fa74
germanusK <germanuskeming@gmail.com>
2023-01-08 15:53:50 +0100

    08/01/2023 3:50am

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'u359014596_WordpressOne' );
 define( 'DB_PASSWORD', '' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'mantohnasah.com' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
