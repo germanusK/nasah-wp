6d83c3cefbf394774702e4224d3c61f5b6768db8
germanusK <germanuskeming@gmail.com>
2023-01-09 13:22:54 +0100

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'u359014596_WordpressOne' );
 define( 'DB_PASSWORD', '' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'ns1.dns-parking.com' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
